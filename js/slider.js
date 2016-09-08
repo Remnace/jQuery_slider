0856a36075749b981f345b9b8b5743b6759a9cab
Remnace <rileyae20@gmail.com>
2016-09-07 21:19:04 -0500

    Tutorial Part 2 complete

---
+++ js/slider.js
@@ -0,0 +1,26 @@
+sliderint = 1;
+sliderNext = 2;
+
+$(document).ready(function() {
+	$("#slider > img#1").fadeIn(300);
+	startSlider();
+});
+
+function startSlider() {
+	count = $("#slider > img").length;
+
+	loop = setInterval(function() {
+
+		if(sliderNext > count) {
+			sliderNext = 1;
+			sliderint = 1;
+		}
+
+		$("#slider > img").fadeOut(300);
+		$("#slider > img#" + sliderNext).fadeIn(300);
+
+		sliderint = sliderNext;
+		sliderNext = sliderNext+1;
+
+	}, 3000)
+}
